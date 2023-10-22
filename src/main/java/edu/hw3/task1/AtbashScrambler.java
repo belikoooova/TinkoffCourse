23007f3618e5eb85b37c504e26208148ebc4a079
Maria Belikova <mebelikova@edu.hse.ru>
2023-10-22 19:51:43 +0300

    add task1

---
+++ src/main/java/edu/hw3/task1/AtbashScrambler.java
@@ -0,0 +1,27 @@
+package edu.hw3.task1;
+
+import java.util.HashMap;
+
+public class AtbashScrambler {
+    private static HashMap<Character, Character> atbashDictionary = new HashMap<>();
+    private static final int ALPHABET_SIZE = 26;
+
+    public AtbashScrambler() {
+        for (int i = 0; i < ALPHABET_SIZE; ++i) {
+            atbashDictionary.put((char) ('A' + i), (char) ('A' + ALPHABET_SIZE - i - 1));
+            atbashDictionary.put((char) ('a' + i), (char) ('a' + ALPHABET_SIZE - i - 1));
+        }
+    }
+
+    public String atbash(String stringToEncrypt) {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < stringToEncrypt.length(); ++i) {
+            if (atbashDictionary.containsKey(stringToEncrypt.charAt(i))) {
+                stringBuilder.append(atbashDictionary.get(stringToEncrypt.charAt(i)));
+            } else {
+                stringBuilder.append(stringToEncrypt.charAt(i));
+            }
+        }
+        return stringBuilder.toString();
+    }
+}
