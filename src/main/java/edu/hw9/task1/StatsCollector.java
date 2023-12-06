b02d10826b6e781660d0c5d5500b703910c6b615
Maria Belikova <mebelikova@edu.hse.ru>
2023-12-06 22:54:46 +0300

    added task 2

--- src/main/java/edu/hw9/task1/StatsCollector.java
+++ src/main/java/edu/hw9/task1/StatsCollector.java
@@ -1,6 +1,5 @@
 package edu.hw9.task1;
 
-import lombok.SneakyThrows;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
@@ -11,12 +10,14 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
+import lombok.SneakyThrows;
 
+@SuppressWarnings("MultipleStringLiterals")
 public class StatsCollector {
     private static final int OPERATIONS_AMOUNT = 4;
     private static final int TIMEOUT = 60;
-    private static final Map<Command, List<Double>> dataByCommand = new ConcurrentHashMap<>();
-    private static final ExecutorService service = Executors.newFixedThreadPool(OPERATIONS_AMOUNT);
+    private static final Map<Command, List<Double>> DATA_BY_COMMAND = new ConcurrentHashMap<>();
+    private static final ExecutorService SERVICE = Executors.newFixedThreadPool(OPERATIONS_AMOUNT);
 
     public void push(String commandString, double[] data) {
         switch (commandString) {
@@ -41,23 +42,23 @@ public class StatsCollector {
     public Map<String, Double> getStats() {
         Map<String, Double> stats = new ConcurrentHashMap<>();
         try {
-            Future<?> sumFuture = service.submit(() -> putSum(stats));
-            Future<?> meanFuture = service.submit(() -> putMean(stats));
-            Future<?> maxFuture = service.submit(() -> putMax(stats));
-            Future<?> minFuture = service.submit(() -> putMin(stats));
+            Future<?> sumFuture = SERVICE.submit(() -> putSum(stats));
+            Future<?> meanFuture = SERVICE.submit(() -> putMean(stats));
+            Future<?> maxFuture = SERVICE.submit(() -> putMax(stats));
+            Future<?> minFuture = SERVICE.submit(() -> putMin(stats));
 
             sumFuture.get();
             meanFuture.get();
             maxFuture.get();
             minFuture.get();
         } finally {
-            service.shutdown();
+            SERVICE.shutdown();
             try {
-                if (!service.awaitTermination(TIMEOUT, TimeUnit.SECONDS)) {
-                    service.shutdownNow();
+                if (!SERVICE.awaitTermination(TIMEOUT, TimeUnit.SECONDS)) {
+                    SERVICE.shutdownNow();
                 }
-            } catch (InterruptedException ie) {
-                service.shutdownNow();
+            } catch (InterruptedException e) {
+                SERVICE.shutdownNow();
                 Thread.currentThread().interrupt();
             }
         }
@@ -65,7 +66,7 @@ public class StatsCollector {
     }
 
     private void putSum(Map<String, Double> stats) {
-        double sum = dataByCommand
+        double sum = DATA_BY_COMMAND
             .getOrDefault(Command.SUM, Collections.emptyList())
             .stream()
             .reduce(0.0, Double::sum);
@@ -73,7 +74,7 @@ public class StatsCollector {
     }
 
     private void putMean(Map<String, Double> stats) {
-        double mean = dataByCommand
+        double mean = DATA_BY_COMMAND
             .getOrDefault(Command.MEAN, Collections.emptyList())
             .stream()
             .mapToDouble(Double::doubleValue)
@@ -83,7 +84,7 @@ public class StatsCollector {
     }
 
     private void putMin(Map<String, Double> stats) {
-        double min = dataByCommand
+        double min = DATA_BY_COMMAND
             .getOrDefault(Command.MIN, Collections.emptyList())
             .stream()
             .min(Double::compareTo)
@@ -92,7 +93,7 @@ public class StatsCollector {
     }
 
     private void putMax(Map<String, Double> stats) {
-        double max = dataByCommand
+        double max = DATA_BY_COMMAND
             .getOrDefault(Command.MAX, Collections.emptyList())
             .stream()
             .max(Double::compareTo)
@@ -101,8 +102,8 @@ public class StatsCollector {
     }
 
     private void addToMap(Command command, double[] data) {
-        dataByCommand.putIfAbsent(command, new ArrayList<>());
-        dataByCommand.get(command).addAll(Arrays.stream(data).boxed().toList());
+        DATA_BY_COMMAND.putIfAbsent(command, new ArrayList<>());
+        DATA_BY_COMMAND.get(command).addAll(Arrays.stream(data).boxed().toList());
     }
 
     private enum Command {
